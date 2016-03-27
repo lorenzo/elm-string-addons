4185e1c61d990cbaf16d41aa813188ece4eaaeef
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2016-03-27 22:44:09 +0200

    Implemented upperWords

--- src/String/Addons.elm
+++ src/String/Addons.elm
@@ -1,15 +1,37 @@
-module String.Addons (..) where
+module String.Addons (upperFirst, upperWords) where
 
 {-| Additional functions for working with Strings
+
+@docs upperFirst
 -}
 
-import String exposing (uncons, cons)
+import String exposing (uncons, cons, words, join)
 import Char exposing (toUpper)
+import Regex exposing (regex, replace, HowMany(..))
 import Maybe exposing (Maybe(..))
+import List
 
-upperFirst: String -> String
+
+{-| Make a string's first character uppercase
+-}
+upperFirst : String -> String
 upperFirst word =
-    let split = uncons word in
-    case split of
-        Nothing -> ""
-        Just (head, tail) -> cons (toUpper head) tail
+  uncons word
+    |> Maybe.map (\( head, tail ) -> cons (toUpper head) tail)
+    |> Maybe.withDefault ""
+
+
+{-| Uppercase the first character of each word in a string
+-}
+upperWords : String -> String
+upperWords ws =
+  ws
+    |> replace
+        All
+        (regex "^([a-z]+)|\\s+([a-z]+)")
+        (\{ match } -> uppercaseMatch match)
+
+
+uppercaseMatch match =
+  match
+    |> replace All (regex "\\w+") (\{ match } -> upperFirst match)
--- tests/TestRunner.elm
+++ tests/TestRunner.elm
@@ -1,19 +1,20 @@
 module TestRunner (..) where
 
-import String.Addons exposing (upperFirst)
+import String.Addons exposing (upperFirst, upperWords)
 import String exposing (uncons, fromChar, toUpper)
 import Check exposing (Claim, Evidence, suite, claim, that, is, for, true, quickCheck)
-import Check.Producer exposing (string)
+import Check.Producer exposing (string, list, filter)
 import Check.Test
 import ElmTest
+import Debug
 
 
-claims : Claim
-claims =
+upperFirstClaims : Claim
+upperFirstClaims =
   suite
     "upperFirst"
     [ claim
-        "it only converts to uppercase the first char in the string"
+        "It only converts to uppercase the first char in the string"
         `that` (\string -> uncons (upperFirst string) |> Maybe.map fst |> Maybe.map fromChar |> Maybe.withDefault "")
         `is` (\string -> uncons string |> Maybe.map fst |> Maybe.map fromChar |> Maybe.map toUpper |> Maybe.withDefault "")
         `for` string
@@ -25,9 +26,29 @@ claims =
     ]
 
 
+upperWordsClaims : Claim
+upperWordsClaims =
+  suite
+    "upperWords"
+    [ claim
+        "It converts the first letter of each word to uppercase"
+        `that` (\arg -> arg |> String.join " " |> upperWords |> String.words)
+        `is` (\arg -> arg |> String.join " " |> String.words |> List.map upperFirst)
+        `for` list string
+    , claim
+        "It does not change the length of the string"
+        `that` (\arg -> arg |> String.join " " |> upperWords |> String.length)
+        `is` (\arg -> arg |> String.join " " |> String.length)
+        `for` list string
+    ]
+
+
 evidence : Evidence
 evidence =
-  quickCheck claims
+  suite
+    "String.Addons"
+    [ upperFirstClaims, upperWordsClaims ]
+    |> quickCheck
 
 
 main =
